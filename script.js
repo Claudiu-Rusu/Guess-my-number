322adfc13f7e92897a224093f7baed78107a401f
Claudiu-Rusu <94001162+Claudiu-Rusu@users.noreply.github.com>
2022-03-31 15:42:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage("⛔️ No number!");
+
+    // When guess is correct
+  } else if (guess === secretNumber) {
+    displayMessage("🎉 Correct Number!");
+    document.querySelector(".number").textContent = secretNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    // Set personal best
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💥 You lost the game!");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
+
+// Allow eneter key to submit your number
+document.getElementById("guess").addEventListener("keyup", function (event) {
+  if (event.code === "Enter") {
+    event.preventDefault();
+    document.getElementById("abc").click();
+  }
+});
